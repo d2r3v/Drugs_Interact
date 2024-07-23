f46ce3a6eee011c47f83dc3b5259092261365d0d
d2r3v <drv792@gmail.com>
2024-07-22 20:12:27 -0700

    Updated API

--- app.js
+++ app.js
@@ -8,12 +8,44 @@ superagent
   .end((err, res) => {
     // Calling the end function will send the request
   });
+  
+  function snipAtFirstInstance(str, target) {
+    var index = str.indexOf(target);
+    if (index !== -1) {
+        return str.substring(0, index);
+    } else {
+        return str;
+    }
+}
 
+function snipBetween(str, startStr, endStr) {
+  var startIndex = str.indexOf(startStr);
+  var endIndex = str.indexOf(endStr);
+  if (startIndex !== -1 && endIndex !== -1) {
+      return str.substring(startIndex + startStr.length, endIndex);
+  } else {
+      return str;
+  }
+}
 
 (async () => {
   try {
-    const res = await superagent.post('http://api.fda.gov/drug/event.json?search=cetrizine');
-    console.log(res);
+    let res;
+    res = await superagent.post('https://www.drugs.com/cetirizine-hcl.html');
+   //console.log(res.text)
+
+   console.log(typeof res.text)
+
+  console.log(res.text.substring(
+    res.text.indexOf("=") + 1, 
+    res.text.lastIndexOf(".")))
+
+   console.log(snipBetween(res.text,">","content="))
+
+   //console.log(res.text.head)
+
+   
+   
   } catch (err) {
     console.error(err);
   }
